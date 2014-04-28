9ec05a0f1944a42b43deaae1c9fa443095afda3e
Jason Mannon & Jason Hamilton <student@epicodus.com>
2014-04-28 09:32:01 -0700

    initial commit, setting up rails to use ember

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikipages::Application.config.secret_key_base = '0d66a59fc4205e773a0eb5394778a77ecceeacf9d8e32bc3863c975de24c88aac26b2899febeca691135e654ad82befb4408523dd9c41bbb381a176c77390cf0'
